b62c06f921e6478512c77a63cc45789700b9ed23
anjali-segu <asegu01@gmail.com>
2018-07-01 19:16:51 -0500

    set up campaign model

--- charity/admin.py
+++ charity/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 
-from .models import CharityProfile
+from .models import CharityProfile, Campaign, CampaignItem
 
 class CharityProfileAdmin(admin.ModelAdmin):
     list_display = (
@@ -33,5 +33,41 @@ class CharityProfileAdmin(admin.ModelAdmin):
             f'<a href="{instance.profile_url()}">{instance.profile_url()}</a>'
         )
 
+
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        '_charityprofile',
+        'goal',
+        'date_created',
+    )
+
+    def _charityprofile(self, instance):
+        link = reverse('admin:charity_charityprofile_change', args=[instance.charityprofile.id])
+
+        return format_html(
+            f'<a href="{link}" target="_blank">{instance.charityprofile.name}</a>'
+        )
+
+class CampaignItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        '_campaign',
+        'item_name',
+        'item_cost',
+        'item_img_url',
+        'date_created',
+    )
+
+    def _campaign(self, instance):
+        link = reverse('admin:charity_campaign_change', args=[instance.campaign.id])
+
+        return format_html(
+            f'<a href="{link}" target="_blank">{instance.campaign.id}</a>'
+        )
+
+
 # Register your models here.
 admin.site.register(CharityProfile, CharityProfileAdmin)
+admin.site.register(Campaign, CampaignAdmin)
+admin.site.register(CampaignItem, CampaignItemAdmin)
--- charity/models.py
+++ charity/models.py
@@ -2,6 +2,7 @@ import uuid
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Sum
 
 # Create your models here.
 class CharityProfile(models.Model):
@@ -18,3 +19,40 @@ class CharityProfile(models.Model):
 
     def profile_url(self):
         return f'/charity/{self.name}/{self.id}/'
+
+    def campaign_goal(self):
+        campaign = self.campaign_set.all()[0]
+        return int(campaign.goal())
+
+    def campaign_items(self):
+        campaign = self.campaign_set.all()[0]
+        return campaign.campaignitem_set.all()
+
+    def __str__(self):
+        return f'Charity Profile: {self.name} ({self.id})'
+
+
+class Campaign(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    charityprofile = models.ForeignKey(CharityProfile, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def goal(self):
+        """
+        Should be the sum of the CampaignItem cost
+        """
+        total_cost = self.campaignitem_set.all().aggregate(Sum('item_cost'))['item_cost__sum']
+
+        return total_cost
+
+    def __str__(self):
+        return f'Campaign: {self.charityprofile.name} ({self.id})'
+
+class CampaignItem(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
+    item_name = models.CharField(max_length=100)
+    item_cost = models.FloatField()
+    item_description = models.TextField(null=True, blank=True, default=None)
+    item_img_url = models.URLField(null=True, blank=True, default=None)
+    date_created = models.DateTimeField(auto_now_add=True)
