66c3260740ccdf29fa73ab96a2424ca8813d221e
anjali-segu <asegu01@gmail.com>
2018-09-09 22:01:49 -0500

    Dont include release for sentry

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -159,5 +159,5 @@ if int(os.getenv('PROD', 0)):
         'dsn': os.getenv('SENTRY_DSN'),
         # If you are using git, you can also automatically configure the
         # release based on the git info.
-        'release': raven.fetch_git_sha(os.path.abspath(os.pardir)),
+        # 'release': raven.fetch_git_sha(os.path.abspath(os.pardir)),
     }
