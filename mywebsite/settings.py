821b707b24a696aad2fb718740c9a4f450b75332
anjali-segu <asegu01@gmail.com>
2018-10-13 15:32:33 -0500

    changed security settings

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -172,8 +172,12 @@ if int(os.getenv('PROD', 0)):
 
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-    SECURE_HSTS_SECONDS = 60
+    # Number of seconds in 1 year
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
     SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     X_FRAME_OPTIONS = 'DENY'
